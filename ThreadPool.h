59146f99ac7fc212bcaad7570c9ec34dd0fa678b
sai-koushik-macha <saikoushikmacha@gmail.com>
2024-06-22 11:09:40 +0530

    Add clang-format

--- ThreadPool.h
+++ ThreadPool.h
@@ -1,128 +1,129 @@
 #ifndef THREADPOOL_H_
 #define THREADPOOL_H_
 
-#include <deque>
 #include <pthread.h>
 #include <sched.h>
 
+#include <deque>
+
 class ThreadPool {
-public:
-  inline void EnqueTask(void *(*const taskFuncPtr)(void *),
-                        void *const arg) noexcept {
-    pthread_spin_lock(&sp);
-    tasks_queue.emplace_back(taskFuncPtr, arg);
-    pthread_spin_unlock(&sp);
-  }
-
-  inline bool EnqueTaskBasedOnAvailability(void *(*const taskFuncPtr)(void *),
-                                           void *const arg) noexcept {
-    pthread_spin_lock(&sp);
-    if (tasks_queue.size() >= total_threads) {
-      pthread_spin_unlock(&sp);
-      return false;
-    }
-    tasks_queue.emplace_back(taskFuncPtr, arg);
-    pthread_spin_unlock(&sp);
-    return true;
-  }
-
-  ThreadPool(int *cores_, int total_threads_)
-      : cores(cores_), total_threads(total_threads_), core_iter(0),
-        keep_running(true) {
-
-    start_running_tasks = false;
-
-    pthread_spin_init(&sp, PTHREAD_PROCESS_PRIVATE);
-
-    threads = new pthread_t[total_threads];
-
-    for (int i = 0; i < total_threads; i++) {
-      pthread_create(&threads[i], nullptr, thread_run_helper, this);
+   public:
+    inline void EnqueTask(void *(*const taskFuncPtr)(void *),
+                          void *const arg) noexcept {
+        pthread_spin_lock(&sp);
+        tasks_queue.emplace_back(taskFuncPtr, arg);
+        pthread_spin_unlock(&sp);
     }
 
-    while (true) {
-      pthread_spin_lock(&sp);
-      int core_iter_ = core_iter;
-      pthread_spin_unlock(&sp);
-      if (core_iter_ == total_threads) {
-        break;
-      }
+    inline bool EnqueTaskBasedOnAvailability(void *(*const taskFuncPtr)(void *),
+                                             void *const arg) noexcept {
+        pthread_spin_lock(&sp);
+        if (tasks_queue.size() >= total_threads) {
+            pthread_spin_unlock(&sp);
+            return false;
+        }
+        tasks_queue.emplace_back(taskFuncPtr, arg);
+        pthread_spin_unlock(&sp);
+        return true;
     }
 
-    start_running_tasks = true;
-  }
-  ~ThreadPool() {
+    ThreadPool(int *cores_, int total_threads_)
+        : cores(cores_),
+          total_threads(total_threads_),
+          core_iter(0),
+          keep_running(true) {
+        start_running_tasks = false;
 
-    keep_running = false;
+        pthread_spin_init(&sp, PTHREAD_PROCESS_PRIVATE);
 
-    for (int i = 0; i < total_threads; i++) {
-      pthread_join(threads[i], nullptr);
+        threads = new pthread_t[total_threads];
+
+        for (int i = 0; i < total_threads; i++) {
+            pthread_create(&threads[i], nullptr, thread_run_helper, this);
+        }
+
+        while (true) {
+            pthread_spin_lock(&sp);
+            int core_iter_ = core_iter;
+            pthread_spin_unlock(&sp);
+            if (core_iter_ == total_threads) {
+                break;
+            }
+        }
+
+        start_running_tasks = true;
+    }
+    ~ThreadPool() {
+        keep_running = false;
+
+        for (int i = 0; i < total_threads; i++) {
+            pthread_join(threads[i], nullptr);
+        }
+
+        delete[] threads;
+
+        pthread_spin_destroy(&sp);
     }
 
-    delete[] threads;
+   private:
+    struct Task {
+        void *(*const taskFuncPtr)(void *);
+        void *const arg;
 
-    pthread_spin_destroy(&sp);
-  }
+        Task(void *(*const taskFuncPtr_)(void *), void *const arg_)
+            : taskFuncPtr(taskFuncPtr_), arg(arg_) {}
+    };
 
-private:
-  struct Task {
-    void *(*const taskFuncPtr)(void *);
-    void *const arg;
+    volatile bool keep_running;
+    volatile bool start_running_tasks;
+    const int total_threads;
+    int core_iter;
+    pthread_spinlock_t sp;
+    int *cores;
+    pthread_t *threads;
+    std::deque<Task> tasks_queue;
 
-    Task(void *(*const taskFuncPtr_)(void *), void *const arg_)
-        : taskFuncPtr(taskFuncPtr_), arg(arg_) {}
-  };
-
-  volatile bool keep_running;
-  volatile bool start_running_tasks;
-  const int total_threads;
-  int core_iter;
-  pthread_spinlock_t sp;
-  int *cores;
-  pthread_t *threads;
-  std::deque<Task> tasks_queue;
-
-  inline static void *thread_run_helper(void *arg) noexcept {
-    ThreadPool *obj = static_cast<ThreadPool *>(arg);
-    obj->run();
-    return nullptr;
-  }
-
-  inline void AssignCore(int core) noexcept {
-    cpu_set_t cpuset;
-    CPU_ZERO(&cpuset);
-    CPU_SET(core, &cpuset);
-    pthread_setaffinity_np(pthread_self(), sizeof(cpuset), &cpuset);
-  }
-
-  inline void run() noexcept {
-    pthread_spin_lock(&sp);
-    int core = cores[core_iter];
-    core_iter++;
-    pthread_spin_unlock(&sp);
-
-    AssignCore(core_iter);
-
-    while (!start_running_tasks) {
+    inline static void *thread_run_helper(void *arg) noexcept {
+        ThreadPool *obj = static_cast<ThreadPool *>(arg);
+        obj->run();
+        return nullptr;
     }
 
-    while (keep_running) {
-      void *(*taskFuncPtr)(void *);
-      void *arg;
-      bool task_avaiable = false;
-      pthread_spin_lock(&sp);
-      if (tasks_queue.size() > 0) {
-        taskFuncPtr = tasks_queue.front().taskFuncPtr;
-        arg = tasks_queue.front().arg;
-        tasks_queue.pop_front();
-        task_avaiable = true;
-      }
-      pthread_spin_unlock(&sp);
-      if (task_avaiable) {
-        taskFuncPtr(arg);
-      }
+    inline void AssignCore(int core) noexcept {
+        cpu_set_t cpuset;
+        CPU_ZERO(&cpuset);
+        CPU_SET(core, &cpuset);
+        pthread_setaffinity_np(pthread_self(), sizeof(cpuset), &cpuset);
     }
-  }
+
+    inline void run() noexcept {
+        pthread_spin_lock(&sp);
+        int core = cores[core_iter];
+        core_iter++;
+        pthread_spin_unlock(&sp);
+
+        AssignCore(core_iter);
+
+        while (!start_running_tasks) {
+        }
+
+        while (keep_running) {
+            void *(*taskFuncPtr)(void *);
+            void *arg;
+            bool task_avaiable = false;
+            pthread_spin_lock(&sp);
+            if (tasks_queue.size() > 0) {
+                taskFuncPtr = tasks_queue.front().taskFuncPtr;
+                arg = tasks_queue.front().arg;
+                tasks_queue.pop_front();
+                task_avaiable = true;
+            }
+            pthread_spin_unlock(&sp);
+            if (task_avaiable) {
+                taskFuncPtr(arg);
+            }
+        }
+    }
 };
 
 #endif /* THREADPOOL_H_ */
--- main.cpp
+++ main.cpp
@@ -1,90 +1,92 @@
-#include "ThreadPool.h"
+#include <unistd.h>
+
 #include <csignal>
 #include <iostream>
-#include <unistd.h>
+
+#include "ThreadPool.h"
 
 volatile bool run_application;
 
 void *thread_helper(void *arg) {
-  std::cout << " Hi from thread helper" << std::endl;
-  return nullptr;
+    std::cout << " Hi from thread helper" << std::endl;
+    return nullptr;
 }
 
 class Example {
-public:
-  struct ThreadPoolHelper {
-    Example *test;
-    int to_run;
-    ThreadPoolHelper(Example *test_, int to_run_)
-        : test(test_), to_run(to_run_) {}
-  };
+   public:
+    struct ThreadPoolHelper {
+        Example *test;
+        int to_run;
+        ThreadPoolHelper(Example *test_, int to_run_)
+            : test(test_), to_run(to_run_) {}
+    };
 
-  Example(ThreadPool *th_, int k) : th(th_) {
-    t = new ThreadPoolHelper(this, k);
-    AddTaskToQueue();
-  };
-  Example(Example &&) = default;
-  Example(const Example &) = default;
-  Example &operator=(Example &&) = default;
-  Example &operator=(const Example &) = default;
-  ~Example() { delete t; };
+    Example(ThreadPool *th_, int k) : th(th_) {
+        t = new ThreadPoolHelper(this, k);
+        AddTaskToQueue();
+    };
+    Example(Example &&) = default;
+    Example(const Example &) = default;
+    Example &operator=(Example &&) = default;
+    Example &operator=(const Example &) = default;
+    ~Example() { delete t; };
 
-  inline void AddTaskToQueue() noexcept { th->EnqueTask(&Example::run, t); }
+    inline void AddTaskToQueue() noexcept { th->EnqueTask(&Example::run, t); }
 
-private:
-  inline void run_helper(int i) noexcept {
-    switch (i) {
-    case 0:
-      std::cout << "Hi from zero" << std::endl;
-      break;
-    case 1:
-      std::cout << "Hi from one" << std::endl;
-      break;
-    case 2:
-      std::cout << "Hi from two" << std::endl;
-      break;
-    case 3:
-      std::cout << "Hi from three" << std::endl;
-      break;
-    default:
-      std::cout << "Hi given wrong number" << std::endl;
-      break;
+   private:
+    inline void run_helper(int i) noexcept {
+        switch (i) {
+            case 0:
+                std::cout << "Hi from zero" << std::endl;
+                break;
+            case 1:
+                std::cout << "Hi from one" << std::endl;
+                break;
+            case 2:
+                std::cout << "Hi from two" << std::endl;
+                break;
+            case 3:
+                std::cout << "Hi from three" << std::endl;
+                break;
+            default:
+                std::cout << "Hi given wrong number" << std::endl;
+                break;
+        }
+
+        sleep(1);
     }
-
-    sleep(1);
-  }
-  inline static void *run(void *arg) noexcept {
-    auto tph = static_cast<ThreadPoolHelper *>(arg);
-    tph->test->run_helper(tph->to_run);
-    return nullptr;
-  }
-  ThreadPool *th;
-  ThreadPoolHelper *t;
+    inline static void *run(void *arg) noexcept {
+        auto tph = static_cast<ThreadPoolHelper *>(arg);
+        tph->test->run_helper(tph->to_run);
+        return nullptr;
+    }
+    ThreadPool *th;
+    ThreadPoolHelper *t;
 };
 
 void SignalHelper(int signal) {
-  std::cout << " Got kill signal" << std::endl;
-  run_application = false;
+    std::cout << " Got kill signal" << std::endl;
+    run_application = false;
 }
 
 int main(int argc, char *argv[]) {
-  run_application = true;
-  std::signal(SIGINT, SignalHelper);
-  int *cores = new int[2];
-  cores[0] = 0;
-  cores[1] = 1;
-  ThreadPool *th = new ThreadPool(cores, 2);
-  th->EnqueTask(thread_helper, nullptr);
-  Example testing(th, 0);
-  Example testing1(th, 1);
-  Example testing2(th, 2);
-  Example testing3(th, 3);
-  Example testing4(th, 4);
-  std::cout << "Infinite loop starting" << std::endl;
-  while (run_application) {
-  }
-  std::cout << "Thread Pool Deleting" << std::endl;
-  delete th;
-  delete[] cores;
-  return 0;
+    run_application = true;
+    std::signal(SIGINT, SignalHelper);
+    int *cores = new int[2];
+    cores[0] = 0;
+    cores[1] = 1;
+    ThreadPool *th = new ThreadPool(cores, 2);
+    th->EnqueTask(thread_helper, nullptr);
+    Example testing(th, 0);
+    Example testing1(th, 1);
+    Example testing2(th, 2);
+    Example testing3(th, 3);
+    Example testing4(th, 4);
+    std::cout << "Infinite loop starting" << std::endl;
+    while (run_application) {
+    }
+    std::cout << "Thread Pool Deleting" << std::endl;
+    delete th;
+    delete[] cores;
+    return 0;
 }
